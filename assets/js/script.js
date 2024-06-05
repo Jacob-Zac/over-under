101062cadee9297d320472cef82f2e5fb89c9508
Jacob-zac <jacobrz030@gmail.com>
2024-06-04 18:37:14 -0700

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,18 +18,23 @@ const addEventOnElements = function (elements, eventType, callback) {
  * NAVBAR TOGGLE FOR MOBILE
  */
 
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]");
+document.addEventListener('DOMContentLoaded', function() {
+  const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+  const navbar = document.querySelector('[data-navbar]');
+  const overlay = document.querySelector('[data-overlay]');
 
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("nav-active");
-}
+  navTogglers.forEach(toggler => {
+    toggler.addEventListener('click', () => {
+      navbar.classList.toggle('active');
+      overlay.classList.toggle('active');
+    });
+  });
 
-addEventOnElements(navTogglers, "click", toggleNavbar);
-
+  overlay.addEventListener('click', () => {
+    navbar.classList.remove('active');
+    overlay.classList.remove('active');
+  });
+});
 
 
 /**
